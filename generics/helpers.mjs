da3a5457ab9aadb60109a01b8b6e010617b13d73
feldspatt <mathieuantoine4@gmail.com>
2022-12-20 21:12:18 +0100

    coords should works better now.

--- generics/helpers.mjs
+++ generics/helpers.mjs
@@ -2,4 +2,4 @@
  * Give the clicked coordinates, horizontal then vertical. Headers is row 0, and the first column is column 0.
  * @returns {(number|number)[]}
  */
-export const getCellCoords =(event)=> [event.target.cellIndex, event.target.parentElement.rowIndex - 2]
+export const getCellCoords =(event)=> [event.target.cellIndex, event.target.parentElement.rowIndex]
