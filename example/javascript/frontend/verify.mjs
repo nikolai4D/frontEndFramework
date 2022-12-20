31c6420263c76ea3c17bb80f177a85b319eb0019
feldspatt <mathieuantoine4@gmail.com>
2022-12-20 18:52:38 +0100

    updated the get CellCoordsMethod. Removed all the example part as now the minimal example will be the point of reference.

--- example/javascript/frontend/verify.mjs
+++
@@ -1,7 +0,0 @@
-import {credentials} from "../index.mjs";
-
-export function verify(form){
-        const inputValues = form.getValues()
-
-        return inputValues[0] === credentials.username && inputValues[1] === credentials.password
-}
