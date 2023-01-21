bc390b0ec7ce9b393efb631c7154d3915a0009af
Maja Frankiewicz <majafrank@live.se>
2023-01-21 22:47:17 +0100

    feat: added atom to molecule

--- generics/components/molecules/Molecule_Logo.mjs
+++ generics/components/molecules/Molecule_Logo.mjs
@@ -1,6 +1,7 @@
 import {Component} from "../../../core/Component.mjs";
 import {slot} from "../../../core/helpers.mjs";
-
+import {Atom_Heading1} from "../atoms/Atom_Heading1.mjs";
+import {Atom_Icon} from "../atoms/Atom_Icon.mjs";
 
 export function Molecule_Logo() {
     Component.call(this)
@@ -18,6 +19,13 @@ export function Molecule_Logo() {
     }
 
     this.bindScript= function() {
+        let icon = new Atom_Icon()
+        let heading1 = new Atom_Heading1()
+        this.fillSlot("icon", icon.getElement());
+        this.fillSlot("heading1", heading1.getElement());
+
+
+
 
     }
 
