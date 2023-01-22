5ae5a9c92a6657eb2ba16a40877591eb866775e7
Maja Frankiewicz <majafrank@live.se>
2023-01-22 19:07:37 +0100

    fix: added path

--- core/actions/mutations/mutation_set_view_state.mjs
+++ core/actions/mutations/mutation_set_view_state.mjs
@@ -1,4 +1,4 @@
-import { State } from "./State.mjs";
+import { State } from "../State.mjs";
 
 export function SET_VIEWSTATE_TO_STATE(template_model){
     State.views[template_model.view] = template_model
