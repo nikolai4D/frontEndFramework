2705a8830b91fc0a7f62c5d10030c36818326df2
Maja Frankiewicz <majafrank@live.se>
2023-01-22 22:37:40 +0100

    feat: added template files

---
+++ generics/components/templates/Template_Loggedin_View.mjs
@@ -0,0 +1,29 @@
+import {Component} from "../../../core/Component.mjs";
+import {slot} from "../../../core/helpers.mjs";
+
+export function Template_Loggedin() {
+    Component.call(this)
+
+    this.getHtml = function() {
+
+        return `
+            <div class="grid grid__c3r3 template_start">
+                <div class="grid-placement__c1r1-3">
+                    ${slot("organismStartInfo")}
+                </div>
+                <div class="grid-placement__c2-3r2">
+                    ${slot("organismLoginOrSignup")}
+                </div>
+            </div>
+        `
+    }
+
+    this.bindScript= function() {
+        let model = State.views[view].components
+        let organismStartInfo = new Organism_StartInfo(model.organism_startInfo);
+        let organismLoginOrSignup = new Organism_LoginOrSignup(model.organism_loginOrSignup);
+
+        this.fillSlot("organismStartInfo", organismStartInfo.getElement());
+        this.fillSlot("organismLoginOrSignup", organismLoginOrSignup.getElement());
+    };
+}
