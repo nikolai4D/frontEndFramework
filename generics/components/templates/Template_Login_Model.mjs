ed6e00ea02b6e9a74f4d51a895fc2aadb768c237
Nicholas Alvén <nicholas@nikolaidev.com>
2023-01-23 21:31:22 +0100

    test components var

--- generics/components/templates/Template_Login_Model.mjs
+++ generics/components/templates/Template_Login_Model.mjs
@@ -1,19 +1,14 @@
 import { SEND_VIEWSTATE_TO_STATE } from "../../../core/actions/action_send_view_state.mjs";
 import { Template_Login_View } from "./Template_Login_View.mjs";
 
+export function Template_Login_Model(model) {
+  const template_model1 = new Template_Login_View(model.view).props;
+  console.log(template_model1, "template_model1");
 
+  const template_model = {
+    view: model.view,
+    components: { template_model1 },
+  };
 
-export function Template_Login_Model(model) { 
-    const template_model1 = new Template_Login_View(model.view).props;
-    console.log(template_model1, "template_model1")
-    
-    const template_model = {
-            view: model.view,
-               components: { template_model1}
-            
-        }
-
-    SEND_VIEWSTATE_TO_STATE(template_model)
-
-    
+  SEND_VIEWSTATE_TO_STATE(template_model);
 }
