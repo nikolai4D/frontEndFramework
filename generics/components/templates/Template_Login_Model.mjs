84925393a8e22e954fad1e7b8baff89be0c3c26c
Nicholas Alvén <nicholas@nikolaidev.com>
2023-01-23 21:57:56 +0100

    fix: restored

--- generics/components/templates/Template_Login_Model.mjs
+++ generics/components/templates/Template_Login_Model.mjs
@@ -2,12 +2,40 @@ import { SEND_VIEWSTATE_TO_STATE } from "../../../core/actions/action_send_view_
 import { Template_Login_View } from "./Template_Login_View.mjs";
 
 export function Template_Login_Model(model) {
-  const template_model1 = new Template_Login_View(model.view).props;
-  console.log(template_model1, "template_model1");
-
   const template_model = {
     view: model.view,
-    components: { template_model1 },
+    components: {
+      organism_startInfo: {
+        molecule_logo: {
+          atom_icon: {
+            icon: model.logoIcon,
+          },
+          atom_heading4: {
+            text: model.logoText,
+          },
+        },
+        molecule_headerAndText: {
+          atom_heading4: {
+            text: model.startHeading,
+          },
+          atom_text1: {
+            text: model.startText,
+          },
+        },
+      },
+      organism_loginForm: {
+        molecule_loginForm: {
+          atom_input: {
+            type: model.inputType,
+            placeholder: model.inputPlaceholder,
+          },
+          atom_buttonPositive: {
+            text: model.buttonPositive.text,
+            onClick: model.buttonPositive.onClick,
+          },
+        },
+      },
+    },
   };
 
   SEND_VIEWSTATE_TO_STATE(template_model);
