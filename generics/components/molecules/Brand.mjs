cb43e85583e13b04f0f32fcfce842838cca6f261
mathieu <mathieuantoine4@gmail.com>
2023-01-23 15:22:50 +0100

    .

---
+++ generics/components/molecules/Brand.mjs
@@ -0,0 +1,25 @@
+import {Component} from "../../../core/Component.mjs";
+import {Icon} from "../atoms/Icon.mjs";
+import {slot} from "../../../core/helpers.mjs";
+
+
+export function Brand( data ){
+    Component.call(this)
+
+    let icon = new Icon(data.icon)
+
+    this.getHtml = function() {
+        return `
+            <div class="brand">
+                ${slot("icon")}
+                <span>${data.name}</span>
+            </div>
+        `
+    }
+
+    this.bindScript = function() {
+        this.fillSlot("icon", icon.getElement())
+        this.getElement().addEventListener("click", data.onClick)
+    }
+
+}
