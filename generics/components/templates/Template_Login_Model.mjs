a19d90cea66bbb740c42b1b312ca2643b57725af
Maja Frankiewicz <majafrank@live.se>
2023-01-23 17:15:24 +0100

    fix: model

--- generics/components/templates/Template_Login_Model.mjs
+++ generics/components/templates/Template_Login_Model.mjs
@@ -4,10 +4,10 @@ import { Template_Login_View } from "./Template_Login_View.mjs";
 
 
 export function Template_Login_Model(model) { 
-    const template_model1 = Template_Login_View.props;
+    const template_model1 = Template_Login_View(model.view).props;
     console.log(template_model1, "template_model1")
     
-const template_model = {
+    const template_model = {
             view: model.view,
             components: {
                 organism_startInfo : {
