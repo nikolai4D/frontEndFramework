3cd1f846ffc9479c4ba7f73109e22360090bfdf0
Stephannie <st.medenilla@gmail.com>
2023-01-24 13:40:49 +0100

    temporary adding molecule card to organism loginOrSignup

--- generics/components/organisms/Organism_LoginOrSignup.mjs
+++ generics/components/organisms/Organism_LoginOrSignup.mjs
@@ -1,6 +1,7 @@
 import {Component} from "../../../core/Component.mjs";
 import {slot} from "../../../core/helpers.mjs";
 import {Molecule_LoginOrSignup} from "../molecules/Molecule_LoginOrSignup.mjs";
+import { Molecule_Card } from "../molecules/Molecule_Card.mjs";
 
 export function Organism_LoginOrSignup(model) {
     Component.call(this)
@@ -11,6 +12,7 @@ export function Organism_LoginOrSignup(model) {
             <div class="grid__c4r1 organism_login-or-signup">
                 <div class="grid-placement__c2-3r1 center">
                     ${slot("loginOrSignup")}
+                    ${slot("card")}
                 </div>
             </div>
         `
@@ -18,6 +20,8 @@ export function Organism_LoginOrSignup(model) {
 
     this.bindScript= function() {
         let loginOrSignup = new Molecule_LoginOrSignup(model.molecule_loginOrSignup)
+        let card = new Molecule_Card(model.molecule_card)
         this.fillSlot("loginOrSignup", loginOrSignup.getElement());
+        this.fillSlot("card", card.getElement());
     }
 }
