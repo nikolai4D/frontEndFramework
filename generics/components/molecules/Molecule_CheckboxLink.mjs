7bb45e31579dc8dc5616373c6722efedb7ab20f7
Joakim Sjöström <8036138+J0kk3@users.noreply.github.com>
2023-01-27 10:26:20 +0100

    button-layout_start/login/register-view&components

---
+++ generics/components/molecules/Molecule_CheckboxLink.mjs
@@ -0,0 +1,32 @@
+import { slot } from "../../../core/helpers.mjs";
+import { Component } from "../../../core/Component.mjs";
+import { Atom_Input } from "../atoms/Atom_Input.mjs";
+import { Atom_Text2Link } from "../atoms/Atom_Text1.mjs";
+
+export default function ( model )
+{
+    Component.call( this );
+
+    this.getHtml = function ()
+    {
+        return `
+        <div>
+        ${ slot( "input" ) }
+        ${ slot( "paragraphLink" ) }
+        </div>
+        `;
+    };
+
+    this.bindScript = function ()
+    {
+        let input = new Atom_Input( model.atom_input );
+        let paragraph = new Atom_Text2Link( model.atomLink_input );
+        this.fillSlot( "input", input.getElement() );
+        this.fillSlot( "paragraphLink", paragraph.getElement() );
+    };
+
+    this.getValue = function ()
+    {
+        return input.getElement().value;
+    };
+}
--- generics/components/molecules/Molecule_SignupForm.mjs
+++ generics/components/molecules/Molecule_SignupForm.mjs
@@ -1,7 +1,5 @@
 import {Component} from "../../../core/Component.mjs";
 import {slot} from "../../../core/helpers.mjs";
-import { Atom_ButtonPositive } from "../atoms/Atom_ButtonPositive.mjs";
-import { Atom_Text1 } from "../atoms/Atom_Text1.mjs";
 import { Atom_ButtonNeutral } from "../atoms/Atom_ButtonNeutral.mjs";
 import { Atom_Input } from "../atoms/Atom_Input.mjs";
 
@@ -13,6 +11,7 @@ export function Molecule_SignupForm(model) {
         return `
             <div class="molecule_signup-form">
             ${slot("input")}
+            ${slot("molecule-checkbox")}
             ${slot("button-neutral")}
             </div>
         `
@@ -20,8 +19,10 @@ export function Molecule_SignupForm(model) {
 
     this.bindScript= function() {
         let input = new Atom_Input(model.atom_input)
+        let molecultCheckbox =  new Atom_Input(model.atom_checkbox)
         let buttonNeutral = new Atom_ButtonNeutral(model.atom_buttonNeutral)
         this.fillSlot("input", input.getElement())
+        this.fillSlot("molecule-", molecultCheckbox.getElement());
         this.fillSlot("button-neutral", buttonNeutral.getElement());
     }
 
