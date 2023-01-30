06ae92471b83e2dd561e72508ae4d9c396c5cfd4
Stephannie <st.medenilla@gmail.com>
2023-01-30 14:25:31 +0100

    adding basic button

--- generics/components/templates/Template_SearchResult_View.mjs
+++ generics/components/templates/Template_SearchResult_View.mjs
@@ -3,6 +3,7 @@ import { State } from "nd_frontend/core/actions/State.mjs";
 import { Atom_Heading4 } from "nd_frontend/generics/components/atoms/Atom_Heading4.mjs"
 import { slot } from "nd_frontend/core/helpers.mjs";
 import { Organism_Navbar } from "../organisms/Organism_Navbar.mjs"
+import { Atom_ButtonBasic } from "../atoms/Atom_ButtonBasic.mjs";
 
 export function Template_SearchResult_View(view){
     
@@ -12,6 +13,7 @@ export function Template_SearchResult_View(view){
         return `<div>
         ${slot("organismNavbar")}
         ${slot("atomHeader")}
+        ${slot("button")}
         </div>`
     }
 
@@ -19,8 +21,10 @@ export function Template_SearchResult_View(view){
         let model = State.views[view].components;
         let organismNavbar = new Organism_Navbar(model.organism_navbar)
         let atom_heading4 = new Atom_Heading4(model.atom_heading4)
+        let button = new Atom_ButtonBasic(model.atom_button)
 
         this.fillSlot("organismNavbar", organismNavbar.getElement())
         this.fillSlot("atomHeader", atom_heading4.getElement())
+        this.fillSlot("button", button.getElement())
     }
 }
