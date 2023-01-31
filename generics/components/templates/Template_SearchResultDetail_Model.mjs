2b92609e5f0ab4e10bc994f1e52aa4d71094a721
mattias <mattias@nikdev.io>
2023-01-31 17:02:53 +0100

    changed model

--- generics/components/templates/Template_SearchResultDetail_Model.mjs
+++ generics/components/templates/Template_SearchResultDetail_Model.mjs
@@ -5,23 +5,26 @@ export function Template_SearchResultDetail_Model(model) {
             view: model.view,
             components: {
                 headerAndText : {
-                    atom_heading4 : { text: "Heading" },
-                    atom_text1 : { text: "text" },
+                    atom_heading4 : { text: model.headAndTextHeading },
+                    atom_text1 : { text: model.headAndTextText },
                 },
-                atom_image: { imgSrc: "imgSrc", alt: "alt" },
+                atom_image: { imgSrc: model.imgSrc, alt: model.imgAlt },
                 molecule_headerAndList1 : {
-                    atom_heading4 : { text: "Heading1" },
-                    items : [{ text: "li text", icon: "bi bi-check icon-style" }, { text: "li text2", icon: "bi bi-check icon-style" }],
+                    atom_heading4 : { text: model.headerAndList1Heading },
+                    items : [{ text: model.headerAndList1Item1, icon: model.headerAndList1Icon1 }, 
+                             { text: model.headerAndList1Item2, icon: model.headerAndList1Icon2 }],
                 },
                 molecule_headerAndList2 : {
-                    atom_heading4 : { text: "Heading2" },
-                    items : [{ text: "li text", icon: "bi bi-check icon-style" }, { text: "li text2", icon: "bi bi-check icon-style" }],
+                    atom_heading4 : { text: model.headerAndList2Heading },
+                    items : [{ text: model.headerAndList2Item1, icon: model.headerAndList2Icon1 }, 
+                             { text: model.headerAndList2Item2, icon: model.headerAndList2Icon2 }],
                 },
                 molecule_headerAndList3 : {
-                    atom_heading4 : { text: "Heading3" },
-                    items : [{ text: "li text", icon: "bi bi-check icon-style" }, { text: "li text2", icon: "bi bi-check icon-style" }],
+                    atom_heading4 : { text: model.headerAndList1Heading },
+                    items : [{ text: model.headerAndList3Item1, icon: model.headerAndList3Icon1 }, 
+                             { text: model.headerAndList3Item2, icon: model.headerAndList3Icon2 }],
                 },
-                button : { text: "buttonText", onClick: null },
+                button : { text: model.buttonText, onClick: model.buttonOnClick },
             }
         }
 
