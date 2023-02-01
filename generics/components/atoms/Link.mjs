38e93ce7018b80ed91dea7e6819e2e758b1e66e7
Stephannie <st.medenilla@gmail.com>
2023-02-01 09:39:09 +0100

    setting onClick in Link component

--- generics/components/atoms/Link.mjs
+++ generics/components/atoms/Link.mjs
@@ -1,14 +1,16 @@
 import {Component} from "../../../core/Component.mjs";
-// import { router } from "../../../../UrbanCloud-alt1/src/javascript/index.mjs";
+import { router } from "../../../../UrbanCloud-alt1/src/javascript/index.mjs";
 
 export function Link(data){
     Component.call(this)
+
+    this.onClick = this.onClick
 
     this.getHtml = function(){
         return `<a  class="${data.class}">${data.text}</a>`
     }
 
     this.bindScript = function(){
-        this.element.addEventListener("click", ()=> router.goTo(data.route))
+        this.element.addEventListener("click", this.onClick(data.route))
     }
 }
