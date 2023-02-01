3e4157cba4ecabbfbe36ce6ae11d10011ae7df6f
Stephannie <st.medenilla@gmail.com>
2023-02-01 09:41:13 +0100

    new onClick modification

--- generics/components/atoms/Link.mjs
+++ generics/components/atoms/Link.mjs
@@ -4,13 +4,13 @@ import { router } from "../../../../UrbanCloud-alt1/src/javascript/index.mjs";
 export function Link(data){
     Component.call(this)
 
-    this.onClick = this.onClick
+    this.onClick = onClick
 
     this.getHtml = function(){
         return `<a  class="${data.class}">${data.text}</a>`
     }
 
     this.bindScript = function(){
-        this.element.addEventListener("click", this.onClick(data.route))
+        this.element.addEventListener("click", this.data.onClick)
     }
 }
