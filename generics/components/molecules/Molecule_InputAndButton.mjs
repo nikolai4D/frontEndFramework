4250d6cc813819184b4985566bd6371486e80522
Stephannie <st.medenilla@gmail.com>
2023-02-02 10:58:21 +0100

    new functionality added to molecule input and button

--- generics/components/molecules/Molecule_InputAndButton.mjs
+++ generics/components/molecules/Molecule_InputAndButton.mjs
@@ -6,6 +6,13 @@ import {Atom_Input} from "../atoms/Atom_Input.mjs";
 export function Molecule_InputAndButton(model) {
     Component.call(this)
 
+    this.props = {
+        atom_input: new Atom_Input().props,
+        atom_buttonBasic: new Atom_ButtonBasic().props
+    }
+
+    console.log( this.props, "Molecule_InputAndButton")
+
     this.getHtml = function() {
 
         return `
