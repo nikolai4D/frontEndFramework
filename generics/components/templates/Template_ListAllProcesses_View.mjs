77178a8217463643137488edcf805458ff209447
Stephannie <st.medenilla@gmail.com>
2023-02-03 08:33:14 +0100

    temporary adding new slot for third molecule list

--- generics/components/templates/Template_ListAllProcesses_View.mjs
+++ generics/components/templates/Template_ListAllProcesses_View.mjs
@@ -20,8 +20,11 @@ export function Template_ListAllProcesses_View(view){
                         ${slot("heading")}
                         ${slot("mol_Input_Btn")}
                     </div>
-                    ${slot("moleculeList1")}
-                    ${slot("moleculeList4")}
+                    <div class="listItems_container">
+                        ${slot("moleculeList1")}
+                        ${slot("moleculeList2")}
+                        ${slot("moleculeList3")}
+                    </div>
                 </div>
                 ${slot("verticalScroll")}
             </div>
@@ -34,22 +37,15 @@ export function Template_ListAllProcesses_View(view){
         let heading = new Atom_Heading4(model.atom_heading4)
         let mol_inputBtn = new Molecule_InputAndButton(model.molecule_inputAndButton)
         let mol_list1 = new Molecule_List(model.molecule_list1)
-        // let mol_list2 = new Molecule_ListNoHeading(model.molecule_list1)
-        // let mol_list3 = new Molecule_ListNoHeading(model.molecule_list1)
-        let mol_list4 = new Molecule_List(model.molecule_list2)
-        // let mol_list5 = new Molecule_ListNoHeading(model.molecule_list2)
-        // let mol_list6 = new Molecule_ListNoHeading(model.molecule_list2)
+        let mol_list2 = new Molecule_List(model.molecule_list2)
+        let mol_list3 = new Molecule_List(model.molecule_list3)
+
 
         this.fillSlot("organismNavbar", organismNavbar.getElement())
         this.fillSlot("heading", heading.getElement())
         this.fillSlot("mol_Input_Btn", mol_inputBtn.getElement())
-
         this.fillSlot("moleculeList1", mol_list1.getElement())
-        // this.fillSlot("moleculeList2", mol_list2.getElement())
-        // this.fillSlot("moleculeList3", mol_list3.getElement())
-        this.fillSlot("moleculeList4", mol_list4.getElement())
-        // this.fillSlot("moleculeList5", mol_list5.getElement())
-        // this.fillSlot("moleculeList6", mol_list6.getElement())
-
+        this.fillSlot("moleculeList2", mol_list2.getElement())
+        this.fillSlot("moleculeList3", mol_list3.getElement())
     }
 }
