0f11d0458e73f6915ea7e4f405739434d113e182
Stephannie <st.medenilla@gmail.com>
2023-02-06 14:11:40 +0100

    styling content

--- generics/components/molecules/Molecule_ModalSearchResultDetail.mjs
+++ generics/components/molecules/Molecule_ModalSearchResultDetail.mjs
@@ -8,38 +8,27 @@ export function Molecule_ModalSearchResultDetail(model) {
   this.content = model.content;
   this.modal = null;
 
-//   this.primary = model.primaryButton
-
   this.getHtml = function () {
     return `
         <div class="modal-container">
                 <div class="modal-title-section">
                     <div class="upper-section">
-                        <h4 class="modal-title">${model.title}</h4>
                         <i class="bi bi-x"></i>
                     </div>
                 </div> 
-                <hr>
                     ${slot("content")}
-                  
-                <hr>
         </div>
       `;
   };
   
   this.bindScript = function () {
 
-    this.content = new Organism_SearchResultDetail(model.organism_searchResultDetail)
+    this.content = new Organism_SearchResultDetail(model.content.organism_searchResultDetail)
 
-    // this.fillSlot("primary", this.primary.getElement());
     this.fillSlot("content", this.content.getElement());
     
     this.getElement().querySelector(".bi-x").addEventListener("click", () => {
       document.querySelectorAll('.modal')[0].remove()
       });
-      
-    // this.primary.getElement().addEventListener("click", () => {
-    //   document.querySelectorAll('.modal')[0].remove()
-    // });
   };
 }
