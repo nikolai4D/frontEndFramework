2446f54edb40688f835158151155a02ffa41e2d8
Stephannie <st.medenilla@gmail.com>
2023-02-06 14:13:35 +0100

    passing data typo

--- generics/components/molecules/Molecule_ModalSearchResultDetail.mjs
+++ generics/components/molecules/Molecule_ModalSearchResultDetail.mjs
@@ -13,22 +13,27 @@ export function Molecule_ModalSearchResultDetail(model) {
         <div class="modal-container">
                 <div class="modal-title-section">
                     <div class="upper-section">
+                        <h4 class="modal-title">${model.title}</h4>
                         <i class="bi bi-x"></i>
                     </div>
                 </div> 
+                <hr>
                     ${slot("content")}
+                  
+                <hr>
         </div>
       `;
   };
   
   this.bindScript = function () {
 
-    this.content = new Organism_SearchResultDetail(model.content.organism_searchResultDetail)
+    this.content = new Organism_SearchResultDetail(model.organism_searchResultDetail)
 
     this.fillSlot("content", this.content.getElement());
     
     this.getElement().querySelector(".bi-x").addEventListener("click", () => {
       document.querySelectorAll('.modal')[0].remove()
       });
+    
   };
 }
