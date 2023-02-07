227397bafc1bd2300181077bd125b99a63cd1709
Stephannie <st.medenilla@gmail.com>
2023-02-07 13:57:03 +0100

    debugging fillslot

--- generics/components/molecules/Molecule_ModalSearchResultDetail.mjs
+++ generics/components/molecules/Molecule_ModalSearchResultDetail.mjs
@@ -23,13 +23,15 @@ export function Molecule_ModalSearchResultDetail(model) {
                 <div class="org_searh_res_det_btn">
                     ${slot("atom_btnPositive")}
                 </div>
-                <div class="modal">
-                  ${slot("new-modal")}
-                </div>
+                
 
         </div>
       `;
   };
+
+  // <div class="modal">
+  //                 ${slot("new-modal")}
+  //               </div>
 
  
   
@@ -56,7 +58,7 @@ export function Molecule_ModalSearchResultDetail(model) {
       this.modal = new Modal(
         this.content = new Organism_SearchResultDetail(model.organism_searchResultDetail)
       )
-      this.fillSlot("new-modal", this.modal.getElement());
+      // this.fillSlot("new-modal", this.modal.getElement());
       });
   };
 }
