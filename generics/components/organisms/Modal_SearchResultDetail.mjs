503d4d86314f3e28902a1ae4a2980c46e914eacf
Stephannie <st.medenilla@gmail.com>
2023-02-09 11:53:01 +0100

    moving styling part in modal

--- generics/components/organisms/Modal_SearchResultDetail.mjs
+++ generics/components/organisms/Modal_SearchResultDetail.mjs
@@ -4,6 +4,7 @@ import { Atom_ButtonPositive} from "../atoms/Atom_ButtonPositive.mjs"
 import { Organism_SearchResultDetail } from "./Organism_SearchResultDetail.mjs";
 import { Modal } from "./Modal.mjs"
 import { Paragraph } from "../atoms/Paragraph.mjs";
+import { Modal_SearchResultDetail } from "../organisms/"
 
 
 export function Modal_SearchResultDetail(model) {
@@ -38,6 +39,18 @@ export function Modal_SearchResultDetail(model) {
 
     this.bindScript= function() {
 
+        this.content = new Organism_SearchResultDetail(model.organism_searchResultDetail)
+
+        let atom_btnPositive = new Atom_ButtonPositive(model.atom_buttonPositive)
+
+        this.fillSlot("content", this.content.getElement())
+        this.fillSlot("atom_btnPositive", atom_btnPositive.getElement());
+
+        this.getElement().querySelector(".bi-x").addEventListener("click", (e) => {
+            document.querySelectorAll('.modal-container')[0].remove()
+            console.log('cross button pressed')
+        });
+
         const mStyle = this.getElement().style
         mStyle.position = "absolute"
         mStyle.width = window.innerWidth + "px"
@@ -56,20 +69,6 @@ export function Modal_SearchResultDetail(model) {
                 this.getElement().remove()
             }
         })
-
-        this.content = new Organism_SearchResultDetail(model.organism_searchResultDetail)
-
-        let atom_btnPositive = new Atom_ButtonPositive(model.atom_buttonPositive)
-
-        this.fillSlot("content", this.content.getElement())
-        this.fillSlot("atom_btnPositive", atom_btnPositive.getElement());
-
-        this.getElement().querySelector(".bi-x").addEventListener("click", (e) => {
-            document.querySelectorAll('.modal-container')[0].remove()
-            console.log('cross button pressed')
-        });
-
-        
 
         this.getElement().querySelector(".org_searh_res_det_btn").addEventListener("click", (e) => {
             console.log('btn-project button pressed')
