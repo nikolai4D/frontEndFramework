07bf1d133c9fedc980bc5a3715065bdd20a97caf
Stephannie <st.medenilla@gmail.com>
2023-02-10 09:47:19 +0100

    removeProperty to modalBG

--- generics/components/organisms/Modal_SearchResultDetail.mjs
+++ generics/components/organisms/Modal_SearchResultDetail.mjs
@@ -46,7 +46,7 @@ export function Modal_SearchResultDetail(model) {
             document.querySelectorAll('.modal-container')[0].remove()
             console.log('cross button pressed')
             const modalBg = document.getElementById('modal-background')
-            modalBg.style.backgroundColor = "white"
+            modalBg.removeProperty('background-color')
 
         });
 
