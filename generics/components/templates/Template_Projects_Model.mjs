22c8a3775c32e3569be1a5b26ad17fca690c5b6d
Stephannie <st.medenilla@gmail.com>
2023-02-13 13:57:02 +0100

    debug error undefined in molecule headingAboutImage

--- generics/components/templates/Template_Projects_Model.mjs
+++ generics/components/templates/Template_Projects_Model.mjs
@@ -246,7 +246,6 @@ export function Template_Projects_Model ( model )
           },
         ]
       },
-      content : {
         organism_projectInfo :{
           molecule_headingAboutImage : {
             atom_heading4 : {
@@ -293,7 +292,7 @@ export function Template_Projects_Model ( model )
             onClick : model.buttonPositive.onClick
           }
         }
-      }
+      
       
     }
   };
