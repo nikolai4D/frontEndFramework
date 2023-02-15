e6606d25bb990f91c28197551581a3e94013ffc7
Stephannie <st.medenilla@gmail.com>
2023-02-15 12:24:53 +0100

    adding modal to organism list all

--- generics/components/organisms/Organism_ListAll.mjs
+++ generics/components/organisms/Organism_ListAll.mjs
@@ -4,6 +4,7 @@ import {Atom_Input} from "../atoms/Atom_Input.mjs";
 import {Atom_ButtonPositive} from "../atoms/Atom_ButtonPositive.mjs";
 import {Molecule_List} from "../molecules/Molecule_List.mjs";
 import {Atom_Heading2} from "../atoms/Atom_Heading2.mjs";
+import { Modal_ProcessListAll } from "./Modal_ProcessListAll.mjs";
 
 export function Organism_ListAll (model){
 
@@ -52,20 +53,20 @@ export function Organism_ListAll (model){
         let listComponent3 = new Molecule_List(model.lists3)
         this.fillSlot('list3', listComponent3.getElement())
 
-        // this.getElement().querySelector(".overflow-container").addEventListener("click", (e) => {
-        //     console.log('btn-project button pressed')     
+        this.getElement().querySelector(".organism_list-all-search__lists").addEventListener("click", (e) => {
+            console.log('btn-project button pressed')     
 
-        //     const modalId = document.getElementById('modal-processView')
+            // const modalId = document.getElementById('modal-processView')
             
-        //     modalId.innerHTML = `
-        //         <div>
-        //             ${slot("new-modal")}
-        //         </div>
-        //         `
-        //     this.modal = new Modal_SearchResultDetail(model.content)
+            // modalId.innerHTML = `
+            //     <div>
+            //         ${slot("new-modal")}
+            //     </div>
+            //     `
+            // this.modal = new Modal_ProcessListAll(model.content)
 
-        //     this.fillSlot("new-modal", this.modal.getElement());
-        // });
+            // this.fillSlot("new-modal", this.modal.getElement());
+        });
 
     }
 }
