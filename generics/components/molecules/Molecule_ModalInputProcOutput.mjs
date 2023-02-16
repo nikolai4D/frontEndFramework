f1eea408ec3a09d3d709d5a32ebf0fde3d7e2f16
Stephannie <st.medenilla@gmail.com>
2023-02-16 13:32:47 +0100

    removing unused code

--- generics/components/molecules/Molecule_ModalInputProcOutput.mjs
+++ generics/components/molecules/Molecule_ModalInputProcOutput.mjs
@@ -1,9 +1,7 @@
 import {Component} from "../../../core/Component.mjs";
 import {slot} from "../../../core/helpers.mjs";
-import { Atom_Text1} from "../atoms/Atom_Text1.mjs";
 import { Atom_Icon } from "../atoms/Atom_Icon.mjs";
 import { Molecule_List } from "./Molecule_List.mjs";
-import { Atom_Heading4 } from "../atoms/Atom_Heading4.mjs";
 import { Molecule_HeaderAndText } from "../molecules/Molecule_HeaderAndText.mjs"
 
 export function Molecule_ModalInputProcOutput(model) {
@@ -22,11 +20,6 @@ export function Molecule_ModalInputProcOutput(model) {
         `
     }
 
-//     <div class="molecule_process_bigBox">
-//     ${slot("box2")}
-//     ${slot("box2b")}
-// </div>
-
     this.bindScript= function() {
         let box1 = new Molecule_List(model.lists1)
         this.fillSlot("box1", box1.getElement());
@@ -36,12 +29,6 @@ export function Molecule_ModalInputProcOutput(model) {
 
         let box2 = new Molecule_HeaderAndText(model.molecule_headerAndText)
         this.fillSlot("box2", box2.getElement());
-
-        // let box2 = new Atom_Heading4(model.atom_heading4)
-        // this.fillSlot("box2", box2.getElement());
-
-        // let box2b = new Atom_Text1(model.atom_text1)
-        // this.fillSlot("box2b", box2b.getElement());
 
         let icon2 = new Atom_Icon(model.atom_icon2)
         this.fillSlot("icon2", icon2.getElement());
