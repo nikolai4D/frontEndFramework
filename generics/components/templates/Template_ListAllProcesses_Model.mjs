455019c91dada6b85a371262dc35b73fc01d2c97
Feldspatt <mathieuantoine4@gmail.com>
2023-02-27 10:50:56 +0100

    merged things

--- generics/components/templates/Template_ListAllProcesses_Model.mjs
+++ generics/components/templates/Template_ListAllProcesses_Model.mjs
@@ -45,181 +45,181 @@ export function Template_ListAllProcesses_Model(model) {
                         atom_icon : {
                             icon : model.logoIcon
                         },
-                        atom_heading4 : {
-                            text : model.logoText
+                        atom_heading4: {
+                            text: model.logoText
                         }
                     },
-                    molecule_iconAndLink1 : {
-                        atom_icon : {
-                            icon : model.icon1
+                    molecule_iconAndLink1: {
+                        atom_icon: {
+                            icon: model.icon1
                         },
-                        atom_link : {
-                            text : model.link1.text,
+                        atom_link: {
+                            text: model.link1.text,
                             onClick: model.link1.onClick
                         }
                     },
-                    molecule_iconAndLink2 : {
-                        atom_icon : {
-                            icon : model.icon2
+                    molecule_iconAndLink2: {
+                        atom_icon: {
+                            icon: model.icon2
                         },
-                        atom_link : {
-                            text : model.link2.text,
+                        atom_link: {
+                            text: model.link2.text,
                             onClick: model.link2.onClick
                         }
                     },
-                    molecule_iconAndLink3 : {
-                        atom_icon : {
-                            icon : model.icon3
+                    molecule_iconAndLink3: {
+                        atom_icon: {
+                            icon: model.icon3
                         },
-                        atom_link : {
-                            text : model.link3.text,
+                        atom_link: {
+                            text: model.link3.text,
                             onClick: model.link3.onClick
                         }
                     },
-                    molecule_iconAndLink4 : {
-                        atom_icon : {
-                            icon : model.icon4
+                    molecule_iconAndLink4: {
+                        atom_icon: {
+                            icon: model.icon4
                         },
-                        atom_link : {
-                            text : model.link4.text,
+                        atom_link: {
+                            text: model.link4.text,
                             onClick: model.link4.onClick
                         }
                     },
-                    molecule_iconAndLink5 : {
-                        atom_icon : {
-                            icon : model.icon5
+                    molecule_iconAndLink5: {
+                        atom_icon: {
+                            icon: model.icon5
                         },
-                        atom_link : {
-                            text : model.link5.text,
+                        atom_link: {
+                            text: model.link5.text,
                             onClick: model.link5.onClick
-                            
+
                         }
                     },
-                    molecule_textAndButton : {
-                        atom_text1 : {
-                            text : model.user
+                    molecule_textAndButton: {
+                        atom_text1: {
+                            text: model.user
                         },
-                        atom_button : {
-                            text : model.button.text,
-                            onClick : model.button.onClick
-                        } 
-                    },   
-                    atom_heading4 : {
-                        text : model.user
+                        atom_button: {
+                            text: model.button.text,
+                            onClick: model.button.onClick
+                        }
                     },
-                    atom_buttonPositive : {
-                        text : model.button.text,
-                        onClick : model.button.onClick
+                    atom_heading4: {
+                        text: model.user
                     },
-                    atom_heading4 : {
-                        text : model.user
-                    }, 
+                    atom_buttonPositive: {
+                        text: model.button.text,
+                        onClick: model.button.onClick
+                    }
                 },
-                organism_listAll : {
-                    atom_heading4 : {
-                        text : model.heading
+                organism_listAll: {
+                    atom_heading4: {
+                        text: model.heading
                     },
-                    atom_input : {
-                        type : model.inputType,
-                        placeholder : model.inputPlaceholder
+                    atom_input: {
+                        type: model.inputType,
+                        placeholder: model.inputPlaceholder
                     },
-                    atom_button_positive : {
-                        text : model.buttonNeutral.text,
-                        onClick : model.buttonNeutral.onClick
+                    atom_button_positive: {
+                        text: model.buttonNeutral.text,
+                        onClick: model.buttonNeutral.onClick
                     },
                     lists: [
                         {
-                            atom_heading4 : {
-                                text : model.listHeading1
+                            atom_heading4: {
+                                text: model.listHeading1
                             },
-                            items : model.items
+                            items: model.items
                         },
                         {
-                            atom_heading4 : {
-                                text : model.listHeading2
+                            atom_heading4: {
+                                text: model.listHeading2
                             },
-                            items : model.items
+                            items: model.items
                         },
                         {
-                            atom_heading4 : {
-                                text : model.listHeading3
+                            atom_heading4: {
+                                text: model.listHeading3
                             },
-                            items : model.items
+                            items: model.items
                         }
                     ]
                 },
-                content : {
-                    organism_processModal : {
+                content: {
+                    organism_processModal: {
                         molecule_modalProcParentBoxes: {
-                            atom_heading4a : {
-                                text : model.boxParent1
+                            atom_heading4a: {
+                                text: model.boxParent1
                             },
-                            atom_icon1 : {
-                                icon : model.icon6
+                            atom_icon1: {
+                                icon: model.icon6
                             },
-                            atom_heading4b : {
-                                text : model.boxParent2
+                            atom_heading4b: {
+                                text: model.boxParent2
                             },
-                            atom_icon2 : {
-                                icon : model.icon7
+                            atom_icon2: {
+                                icon: model.icon7
                             },
-                            atom_heading4c : {
-                                text : model.boxParent3
+                            atom_heading4c: {
+                                text: model.boxParent3
                             },
-                            atom_icon3 : {
-                                icon : model.icon8
+                            atom_icon3: {
+                                icon: model.icon8
                             }
                         },
-                        molecule_modalInputProcOutput : {
-                            lists1 : {
-                                atom_heading4 : {
-                                    text : model.listHeading4
+                        molecule_modalInputProcOutput: {
+                            lists1: {
+                                atom_heading4: {
+                                    text: model.listHeading4
                                 },
-                                items : model.items2
+                                items: model.items2
                             },
-                            atom_icon1 : {
-                                icon : model.icon9
+                            atom_icon1: {
+                                icon: model.icon9
                             },
-                            molecule_headerAndText : {
-                                atom_heading4 : {
-                                    text : model.centerText
+                            molecule_headerAndText: {
+                                atom_heading4: {
+                                    text: model.centerText
                                 },
-                                atom_text1 : {
-                                    text : model.centerAbout
+                                atom_text1: {
+                                    text: model.centerAbout
                                 },
                             },
-                            atom_icon2 : {
-                                icon : model.icon10
+                            atom_icon2: {
+                                icon: model.icon10
                             },
-                            lists2 : {
-                                atom_heading4 : {
-                                    text : model.listHeading5
+                            lists2: {
+                                atom_heading4: {
+                                    text: model.listHeading5
                                 },
-                                items : model.items3
+                                items: model.items3
                             }
                         },
-                        molecule_modalChildrenBoxes : {
-                            atom_icon1 : {
-                                icon : model.icon11
+                        molecule_modalChildrenBoxes: {
+                            atom_icon1: {
+                                icon: model.icon11
                             },
-                            atom_heading4a : {
-                                text : model.boxChild1
+                            atom_heading4a: {
+                                text: model.boxChild1
                             },
-                            atom_icon2 : {
-                                icon : model.icon12
+                            atom_icon2: {
+                                icon: model.icon12
                             },
-                            atom_heading4b : {
-                                text : model.boxChild2
+                            atom_heading4b: {
+                                text: model.boxChild2
                             },
-                            atom_icon3 : {
-                                icon : model.icon13
+                            atom_icon3: {
+                                icon: model.icon13
                             },
-                            atom_heading4c : {
-                                text : model.boxChild3
+                            atom_heading4c: {
+                                text: model.boxChild3
                             },
                         }
-            }
-        }
+
+                    }
+                }
+
+            }}
 
     SEND_VIEWSTATE_TO_STATE(template_model)
 }
